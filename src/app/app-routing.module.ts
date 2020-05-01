7854ee366a1a068bfb7439c1042a3fe46c4d701e
Cristian Aldea <aldea.cristian@outlook.com>
2020-05-01 01:11:06 -0400

    Start Website layout and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
+import { ExperiencePageComponent } from './components/experience-page/experience-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
-const routes: Routes = [];
+export const ROUTE_NAMES = {
+  about: 'about',
+  projects: 'projects',
+  experience: 'experience',
+  contact: 'contact',
+};
+const routes: Routes = [
+  { path: ROUTE_NAMES.about, component: AboutPageComponent },
+  { path: ROUTE_NAMES.projects, component: ProjectsPageComponent },
+  { path: ROUTE_NAMES.experience, component: ExperiencePageComponent },
+  { path: ROUTE_NAMES.contact, component: ContactPageComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'project-megaphone';
+  title = 'My Website';
+
+  text = '';
+
+  ngAfterContentInit() {
+    const phrases = [
+      'Welcome to my website!',
+      'Stick around to learn more about me.',
+      'Take care of yourself now. :)',
+    ];
+    let charIndex = 0;
+    let deleting = false;
+    let phraseIndex = 0;
+    let repeat = true;
+
+    const updateText = () => {
+      let wait = 25 + 75 * Math.random();
+
+      if (deleting) {
+        this.text = this.text.substring(0, this.text.length - 1);
+        if (charIndex === 0) {
+          deleting = !deleting;
+          phraseIndex++;
+          wait += 500;
+        } else {
+          charIndex--;
+        }
+      } else {
+        this.text += phrases[phraseIndex][charIndex];
+        if (charIndex === phrases[phraseIndex].length - 1) {
+          deleting = !deleting;
+          wait += 1500;
+          if (phraseIndex == phrases.length - 1) {
+            repeat = false;
+          }
+        } else {
+          charIndex++;
+        }
+      }
+
+      if (repeat) {
+        setTimeout(() => updateText(), wait);
+      }
+    };
+    updateText();
+  }
 }
