9793f29fcb5805d7c6f36ec8261c85cc7d072592
Cristian Aldea <aldea.cristian@outlook.com>
2021-02-16 01:23:44 -0500

    start about page

--- apps/megaphone-ui/src/app/pages/about-page/about-page.tsx
+++ apps/megaphone-ui/src/app/pages/about-page/about-page.tsx
@@ -9,7 +9,19 @@ export function AboutPage(props: AboutPageProps) {
     <div className="section-container about-page-container" id="about-page">
       <div className="section-content-container">
         <div className="page-title">About Me</div>
-        <div>Coming soon!</div>
+        <div className="about-page-info">
+          <img src="assets/images/profile-pic.jpeg" className="about-page-picture" />
+          <p>Hello! I'm current a last-year Software Engineering Co-op student at Concordia.</p>
+          <p>
+            I'm interested in everything software, among other things :). I have a lot of pratical experience
+            in things such as full-stack development, machine learning and cloud native technologies.
+          </p>
+          <p>
+            I love solving problems and learning new things, and I bring consistency and excellence to any
+            project.
+          </p>
+        </div>
+        <p className="about-page-quote">"Today is a gift, that is why we call it the Present."</p>
       </div>
     </div>
   );
