2d51d882d41fab400d401965243c988f50cec1fa
Cristian Aldea <aldea.cristian@outlook.com>
2021-02-28 22:56:31 -0500

    create express app

---
+++ apps/preference-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'preference-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/preference-api',
+};
